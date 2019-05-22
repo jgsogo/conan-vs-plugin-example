c392f7539fcfda17c2e862c1b0edc9bda8507078
Javier G. Sogo <jgsogo@gmail.com>
2019-05-22 18:30:05 +0200

    example

--- app/main.cpp
+++ app/main.cpp
@@ -1,15 +1,9 @@
-#include "lib.h"
 
 #include <iostream>
-#include "Poco/MD5Engine.h"
-#include "Poco/DigestStream.h"
+#include "fmt/format.h"
 
 int main() {
-    Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
-    ds << "abcdefghijklmnopqrstuvwxyz";
-    ds.close();
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+    std::cout << fmt::format("afsdfas") << std::endl;
     return 0;
 }
 
--- lib/lib.cpp
+++ lib/lib.cpp
@@ -1,14 +1,9 @@
 #include "lib.h"
 
 #include <iostream>
-#include "Poco/MD5Engine.h"
-#include "Poco/DigestStream.h"
+#include "fmt/format.h"
 
 int hello() {
-    Poco::MD5Engine md5;
-    Poco::DigestOutputStream ds(md5);
-    ds << "abcdefghijklmnopqrstuvwxyz";
-    ds.close();
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+    std::cout << fmt::format("afsdfas") << std::endl;
 }
 
